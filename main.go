84b2f5ea32c92ca53a74fc7cfa4a941643839e08
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-12-05 19:48:38 +0100

    add simple fileserver
    Signed-off-by: Fernando NUNEZ <fernunezmendoza@gmail.com>

--- main.go
+++ main.go
@@ -9,13 +9,18 @@ func main() {
 
 	serverMux := http.NewServeMux()
 
+	// Create server
 	server := http.Server{
 		Handler: serverMux,
 		Addr:    ":8080",
 	}
+
+	// add handle to / path by looking current directory
+	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	// launch server
 	err := server.ListenAndServe()
 	if err != nil {
-
 		fmt.Printf("Error encountered launching server %v", err)
 		return
 	}
