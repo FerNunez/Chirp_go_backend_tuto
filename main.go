e1d49c1e49b81dbea8c4086518539309a734c617
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-12-05 22:33:45 +0100

    Add readiness Handler +StripPrefix for index
    Signed-off-by: Fernando NUNEZ <fernunezmendoza@gmail.com>

--- main.go
+++ main.go
@@ -15,8 +15,9 @@ func main() {
 		Addr:    ":8080",
 	}
 
-	// add handle to / path by looking current directory
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+	// add handles
+	serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serverMux.HandleFunc("/healthz", ReadinessHandler)
 
 	// launch server
 	err := server.ListenAndServe()
@@ -25,4 +26,11 @@ func main() {
 		return
 	}
 
+}
+
+// Readiness: sends a OK
+func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) // I did 200 instead of http.StatusOk
+	w.Write([]byte("OK"))
 }
