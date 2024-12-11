298b0efcca0c012a402255c289dc2cd2def30837
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-12-11 19:42:54 +0100

    add metrics as html
    Signed-off-by: Fernando NUNEZ <fernunezmendoza@gmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
@@ -14,10 +15,12 @@ func main() {
 		Handler: serverMux,
 		Addr:    ":8080",
 	}
-
+	apiCfg := apiConfig{}
 	// add handles
-	serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	serverMux.HandleFunc("/healthz", ReadinessHandler)
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	serverMux.HandleFunc("GET /api/healthz", ReadinessHandler)
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.NumberOfRequests)
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.ResetNumberOfRequests)
 
 	// launch server
 	err := server.ListenAndServe()
@@ -33,4 +36,43 @@ func ReadinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK) // I did 200 instead of http.StatusOk
 	w.Write([]byte("OK"))
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	// create wrapping handler Func
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment 1
+		cfg.fileserverHits.Add(1)
+		// call wrapped handler
+		next.ServeHTTP(w, r)
+
+	})
+}
+
+func (cfg *apiConfig) NumberOfRequests(w http.ResponseWriter, r *http.Request) {
+	httpResponse := fmt.Sprintf(`
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>
+`, cfg.fileserverHits.Load())
+
+	//w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK) // I did 200 instead of http.StatusOk
+	fmt.Fprintf(w, "%v", httpResponse)
+}
+func (cfg *apiConfig) ResetNumberOfRequests(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK) // I did 200 instead of http.StatusOk
+	fmt.Fprintf(w, "Reset done: %v", cfg.fileserverHits.Load())
 }
